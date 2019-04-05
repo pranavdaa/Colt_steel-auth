fb26cda7471b64d8ed287a61634bd714cd83c5f0
pranavdaa <pranavmaheshwari4321@gmail.com>
2019-04-06 02:24:56 +0530

    First commit

--- app.js
+++ app.js
@@ -19,6 +19,9 @@ app.use(require("express-session")({
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+
+passport.use(new LocalStrategy(User.authenticate()))
 //these methods below  are used for reading the season taking data frm the session and uncoding(deserializeUser) it. and then again coding it(serializeUser)
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
@@ -27,9 +30,53 @@ app.get("/",(req,res)=>{
   res.render("home.ejs")
 })
 
-app.get("/secret",(req,res)=>{
+app.get("/secret",isLoggedIn,(req,res)=>{
   res.render("secret.ejs")
 })
 
+
+// Auth Routes
+app.get("/register",(req,res)=>{
+  res.render("register.ejs")
+})
+
+app.post("/register",(req,res)=>{
+req.body.username
+req.body.password
+
+User.register(new User({username:req.body.username}),req.body.password,function(error,user){
+  if(error){
+    console.log(error);
+     return res.redirect('register');
+  }
+  passport.authenticate("local")(req,res,function(){
+    res.redirect("/secret")
+  });
+} );
+})
+
+
+app.get("/login",(req,res)=>{
+  res.render("login.ejs")
+})
+
+app.post("/login", passport.authenticate("local",{
+  successRedirect: "/secret",
+  failureRedirect: "/login"
+}) ,(req,res)=>{
+  res.send("loged");
+})
+
+app.get("/logout",(req,res)=>{
+  req.logout();
+  res.redirect("/");
+})
+
+function isLoggedIn(req,res,next){
+  if(req.isAuthenticated()){
+  return next();
+  }
+  res.redirect("/login")
+}
 
 app.listen(4000);
